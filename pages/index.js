f150c22628cf9e44c804bb8e22eacbe91a2798dd
Fernando Sanches <ferbs89@gmail.com>
2021-10-19 23:11:39 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Link from 'next/link';
 import { useFetch } from '../hooks/useFetch';
 
 import Layout from '../components/Layout';
-import Layout from '../components/Layout';
 import Loading from '../components/Loading';
 import Error from '../components/Error';
 
